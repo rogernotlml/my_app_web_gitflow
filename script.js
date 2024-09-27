9e4dc99f1dbf72ac571cfa45175d8d2ee7932cfa
rogernotlml <rogernotlml@gmail.com>
2024-09-26 18:20:36 -0600

    hice cambios desde formulario contacto en freacture

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('btnClick').addEventListener('click', function() {
     document.getElementById('output').textContent = '¡Botón clicado!';
 });
-//Este es un nuevo metodo
+//Este es un nuevo metodo listo
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var name = document.getElementById('name').value;
